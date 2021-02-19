94a829d6726c1c483a6210eab80baa4f275efe6d
sudoris <zdent09@gmail.com>
2021-02-19 17:40:16 +0800

    vue test utils review

---
+++ src/components/RandomNumber.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span>{{ randomNumber }}</span>
+    <button @click="setRandomNumber">Generate random number</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    min: {
+      type: Number,
+      default: 1
+    },
+    max: {
+      type: Number,
+      default: 10
+    }
+  },
+  data() {
+    return {
+      randomNumber: 0
+    }
+  },
+  methods: {
+    setRandomNumber() {
+      this.randomNumber = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min
+    }
+  }
+}
+</script>
